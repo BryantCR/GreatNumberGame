97097b5775e7f360647b19772a50f4ea054b9021
BryantCR <bryancvargas20@gmail.com>
2021-09-24 21:31:05 -0600

    all good

--- great_number_game.py
+++ great_number_game.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask import render_template
 from flask import session
 from flask import redirect
+from flask import request
 import random
 
 app = Flask( __name__ )
 app.secret_key = "secret"
-
 
 @app.route( '/', methods=['GET'] )
 def getRandomNumber():
@@ -15,24 +15,30 @@ def getRandomNumber():
         session["guess"] = randomNum
     print(session)
     print(randomNum)
-    return render_template('index.html', randomNum = randomNum)
+    return render_template('/index.html')
 
-@app.route('/user/guess', methods=['POST'])
+@app.route('/guess', methods=['POST'])
 def ok():
-    if userGuess == randomNum:
+    self.getRandomNumber()
+    userGuess1 = int(request.form['userGuesser'])
+    if userGuess1 == randomNum:
         print("Your done, you got it!")
-        return render_template('.html')
-    elif userGuess < randomNum:
+        return render_template('/index.html')
+    elif userGuess1 < randomNum:
         print("Try a little higher")
-        return render_template('.html')
-    elif userGuess > randomNum:
+        return render_template('/index.html')
+    elif userGuess1 > randomNum:
         print("Try a little lower")
-        return render_template('.html')
+        return render_template('/index.html')
     else:
-        
-    return render_template('index.html')
+        return render_template('/index.html')
 
-@app.route( '/destroy_session', methods=['GET'] )
+# @app.route('/guess',methods=['POST'])
+# def userGuess():
+#     session['userGuesser'] = int(request.form['userGuesser'])
+#     return redirect('/')
+
+@app.route( '/reset', methods=['GET'] )
 def closeSession():
     session.clear()
     return redirect( '/' )
